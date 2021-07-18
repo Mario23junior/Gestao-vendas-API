e6fb6da3e8245502ceda0a259d070ed79d63af18
Mario23junior <mariojunior3251@gmail.com>
2021-07-18 02:59:38 -0300

    Criando arquivo gitignore

--- src/main/java/com/vendas/gestaovendas/Service/CategoriaService.java
+++ src/main/java/com/vendas/gestaovendas/Service/CategoriaService.java
@@ -1,7 +1,11 @@
 package com.vendas.gestaovendas.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
+import com.vendas.gestaovendas.model.Categoria;
 import com.vendas.gestaovendas.repository.CategoriaRepository;
 
 @Service
@@ -13,5 +17,11 @@ public class CategoriaService {
 		this.categoriaRepository = categoriaRepository;
  	}
 	
+	public List<Categoria> listAllData(){
+		return categoriaRepository.findAll();
+	}
 	
+	public Optional<Categoria> listByIdData(Long id){
+		return categoriaRepository.findById(id);
+	}
 }
