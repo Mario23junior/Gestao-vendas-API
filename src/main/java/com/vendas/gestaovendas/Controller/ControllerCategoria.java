7003d3d2fae9965d2bb158f3020d571a0f3c42b2
Mario23junior <mariojunior3251@gmail.com>
2021-07-19 02:15:16 -0300

    Salvando dados da tabela categoria do banco de dados

--- src/main/java/com/vendas/gestaovendas/Controller/ControllerCategoria.java
+++ src/main/java/com/vendas/gestaovendas/Controller/ControllerCategoria.java
@@ -3,9 +3,12 @@ package com.vendas.gestaovendas.Controller;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,4 +35,12 @@ public class ControllerCategoria {
 		Optional<Categoria> categoria = categoriaService.listByIdData(codigo);
 		return categoria.isPresent() ? ResponseEntity.ok(categoria) : ResponseEntity.notFound().build();
  	}
+	
+	@PostMapping
+	public ResponseEntity<Categoria> save(@RequestBody Categoria categoria) {
+		 Categoria salvarCategoria = categoriaService.save(categoria);
+		 return ResponseEntity
+				   .status(HttpStatus.CREATED)
+				   .body(salvarCategoria);
+	}
 }
--- src/main/java/com/vendas/gestaovendas/Service/CategoriaService.java
+++ src/main/java/com/vendas/gestaovendas/Service/CategoriaService.java
@@ -24,4 +24,8 @@ public class CategoriaService {
 	public Optional<Categoria> listByIdData(Long id){
 		return categoriaRepository.findById(id);
 	}
+	
+	public Categoria save(Categoria categoria) {
+		return categoriaRepository.save(categoria);
+	}
 }
