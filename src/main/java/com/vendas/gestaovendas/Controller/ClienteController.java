3a6c8a830ef90fa184a217edc7a7240034070047
Mario23junior <mariojunior3251@gmail.com>
2021-08-02 04:01:20 -0300

    Deletando cliente por id

--- src/main/java/com/vendas/gestaovendas/Controller/ClienteController.java
+++ src/main/java/com/vendas/gestaovendas/Controller/ClienteController.java
@@ -8,12 +8,14 @@ import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vendas.gestaovendas.Service.ClienteService;
@@ -65,8 +67,14 @@ public class ClienteController {
 	@PutMapping("/{codigo}")
 	public ResponseEntity<ClienteResponseDTO> atualizar(@PathVariable Long codigo, @Valid @RequestBody ClienteRequestDTO clienteDto){
 	   Cliente clienteAtualizado = clienteService.atualizar(codigo, clienteDto.converterParaEntidade(codigo));
-	   return ResponseEntity.ok(ClienteResponseDTO.converterPataClienteDTO(clienteAtualizado));
-			                
+	   return ResponseEntity.ok(ClienteResponseDTO.converterPataClienteDTO(clienteAtualizado));		                
+	}
+	
+	@ApiOperation(value = "Deletando dados de cliente")
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deletar(@PathVariable Long codigo) {
+		 clienteService.deletar(codigo);
 	}
 }
 
